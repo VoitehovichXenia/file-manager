7ee93caacf33c73a3dc5fd394a1880ed502e66be
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-02 19:18:22 +0300

    feat: add messages on user enterand exit

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,3 +1,6 @@
+const COMMANDS = {
+  exit: '.exit',
+}
 let username = 'anonymus'
 
 const setUserName = () => {
@@ -12,4 +15,19 @@ const setUserName = () => {
   }
 };
 
+const sayHelloToUser = (username) => process.stdout.write(`Welcome to the File Manager, ${username}!\n\n`)
+const sayGoodbyeToUser = (username) => {
+  process.stdout.write(`\n\nThank you for using File Manager, ${username}, goodbye!\n`);
+  process.exit();
+};
+
 setUserName();
+sayHelloToUser(username);
+process.stdin.resume()
+
+process.stdin.on('data', function(chunk, key) {
+  const command = chunk.toString().trim();
+  if (command === COMMANDS.exit) sayGoodbyeToUser(username)
+});
+
+process.on('SIGINT', () => sayGoodbyeToUser(username));
