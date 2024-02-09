3987dc44f509c2f868f981d3641421d6b03ef1a1
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-09 17:12:45 +0300

    fix: add sort to ls command, simplify code

--- src/ls/ls.js
+++ src/ls/ls.js
@@ -2,37 +2,23 @@ import { readdir } from 'node:fs/promises'
 import { DEFAULT_EOL, logOperationFailed } from '../utils.js'
 
 export const ls = async (pathToDir) => {
-  const INDEX_COLUMN_TEXT = '   Index   ';
-  const INDEX_COLUMN_TEXT_LEN = INDEX_COLUMN_TEXT.length;
-  const TYPE_COLUMN_TEXT = '    Type    ';
-  const TYPE_COLUMN_TEXT_LEN = TYPE_COLUMN_TEXT.length;
-  const DIRECTORY_TYPE_TEXT = ' directory  ';
-  const FILE_TYPE_TEXT = ' file       ';
+  const DIRECTORY_TYPE = 'directory';
+  const FILE_TYPE = 'file';
   try {
     const dirContent = await readdir(pathToDir, {
       recursive: false,
       withFileTypes: true
     });
     if (dirContent.length) {
-      const nameColumnLength = Math.max(...dirContent.map(item => item.name.length)) + 2;
-      const rightNameSpace = Math.floor((nameColumnLength - 4) / 2);
-      const leftNameSpace = nameColumnLength - 4 - rightNameSpace;
-      const nameColumnText = `${' '.repeat(rightNameSpace)}Name${' '.repeat(leftNameSpace)}`;
-      const horizontalBorderLength = INDEX_COLUMN_TEXT_LEN + nameColumnLength + TYPE_COLUMN_TEXT_LEN + 3;
-      const horizontalBorder = `${'='.repeat(horizontalBorderLength)}${DEFAULT_EOL}`;
-      const innerBorder = `${'-'.repeat(horizontalBorderLength)}${DEFAULT_EOL}`;
-      process.stdout.write(horizontalBorder);
-      process.stdout.write(`${INDEX_COLUMN_TEXT}|${nameColumnText}|${TYPE_COLUMN_TEXT}|${DEFAULT_EOL}`);
-      process.stdout.write(horizontalBorder);
-      dirContent.forEach((child, index) => {
-        const rightNameSpace = Math.floor((nameColumnLength - child.name.length) / 2);
-        const leftNameSpace = nameColumnLength - child.name.length - rightNameSpace;
-        const indexText = `${index < 10 ? ' '.repeat(5) : index < 100 ? ' '.repeat(4) : ' '.repeat(3)}${index}${index < 10 ? ' '.repeat(5) : index < 100 ? ' '.repeat(5) : ' '.repeat(3)}`;
-        const nameText = `${' '.repeat(rightNameSpace)}${child.name}${' '.repeat(leftNameSpace)}`;
-        const typeText = `${child.isDirectory() ? DIRECTORY_TYPE_TEXT : FILE_TYPE_TEXT}`;
-        process.stdout.write(`${indexText}|${nameText}|${typeText}|${DEFAULT_EOL}`);
-        process.stdout.write(innerBorder);
+      const dirData = []
+      dirContent.forEach(child => {
+        const childData = {}
+        childData.Name = child.name
+        childData.Type = `${child.isDirectory() ? DIRECTORY_TYPE : FILE_TYPE}`
+        dirData.push(childData)
       });
+      dirData.sort((a) => a.Type === DIRECTORY_TYPE ? -1 : 1)
+      console.table(dirData)
     } else {
       process.stdout.write(`${pathToDir} folder is empty.${DEFAULT_EOL}`);
     }
